399334c5ad5468d6a0bec169f1db6df7e66fa054
DAVIDMISTRI <mistri.david.08@itisdalmine.edu.it>
2026-03-03 13:56:57 +0100

    sigma

---
+++ dungeon/src/main/java/com/mistri/dungeon/DungeonApplication.java
@@ -0,0 +1,13 @@
+package com.mistri.dungeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DungeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DungeonApplication.class, args);
+	}
+
+}
---
+++ dungeon/src/test/java/com/mistri/dungeon/DungeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mistri.dungeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DungeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
